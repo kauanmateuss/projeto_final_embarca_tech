4656b830e0f9d7cb828397750b12be8c5326b198
kauanmateuss <kamateus77@gmail.com>
2025-02-21 10:54:38 -0300

    Adicionei e configurei pastas e arquivos para manipulação do display e matriz de leds

--- project.c
+++ project.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/clocks.h"
+#include "hardware/pio.h"
+#include "hardware/pwm.h"
+#include "hardware/i2c.h"
+#include "matriz_leds.h"
+#include "inc/ssd1306.h"
 
+// Definição de variaveis globais e macros
 
+#define btnA_pin 5
+#define btnB_pin 6
+#define buzzerA_pin 21
 
 int main()
 {
