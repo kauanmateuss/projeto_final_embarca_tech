2313c8d0bf9c733ba00ec49c96b6c36a77a3efda
kauanmateuss <kamateus77@gmail.com>
2025-02-22 00:48:48 -0300

    Adicionei interrupções e suas funcionalidades, temporizadores

--- project.c
+++ project.c
@@ -9,6 +9,7 @@
 #include "string.h"
 #include <stdlib.h>
 #include <time.h>
+#include "hardware/timer.h"
 
 // Definição de variaveis globais e macros
 
@@ -20,8 +21,10 @@
 PIO pio;
 uint sm;
 
-uint numero_sorteado; 
-uint contador;
+volatile uint numero_sorteado = 0; 
+volatile uint contador = 0;
+volatile bool inicio_jogo = false;
+volatile uint tentativas = 0;  
 
 // Pinos para o display
 const uint I2C_SDA = 14;
@@ -148,76 +151,134 @@ Matriz_leds_config *numeros[] = {&zero, &um, &dois, &tres, &quatro, &cinco, &sei
 char *inicio[8] = {
     "===============",
     "               ",
-    " BEM VINDO AO  ",
     "               ",
-    " JOGO ADIVINHA ",
+    "     JOGO      ",
     "               ",
-    "               ",
-    "==============="
-};
-
-char *instrucaoA[8] = {
-    "===============",
-    "               ",
-    "  O BOTAO A    ",
-    "  INCREMENTA   ",
-    " UM NUMERO NA  ",
-    " MATRIZ DE LEDS",
+    "  ADIVINHACAO  ",
     "               ",
     "==============="
 };
 
-char *instrucaoB[8] = {
+char *aperte[8] = {
     "===============",
-    "               ",
-    "  O BOTAO B    ",
-    "  DECREMENTA   ",
-    " UM NUMERO NA  ",
-    " MATRIZ DE LEDS",
-    "               ",
-    "==============="
-};
-
-char *instrucaoC[8] = {
-    "===============",
-    "               ",
-    "O BOTAO JOYSTIK",
-    "CONFIRMA O     ",
-    "NUMERO DA      ",
-    "MATRIZ DE LEDS ",
-    "               ",
-    "==============="
-};
-
-char *opcao[8] = {
-    "===============",
-    "               ",
-    " SELECIONE UM  ",
-    "    NUMERO     ",
-    "   DE 0 A 9    ",
-    "   NOS LEDs    ",
-    "               ",
+    "=             =",
+    "=   PRECIONE  =",
+    "=      O      =",
+    "=    BOTAO    =",
+    "= DO JOYSTICK =",
+    "=             =",
     "==============="
 };
 
 char *sorteio[8] = {
     "===============",
-    "               ",
-    "NUMERO DE 0 A 9",
-    "               ",
-    "   SORTEADO    ",
-    "               ",
-    "               ",
+    "=             =",
+    "=    NUMERO   =",
+    "=      DE     =",
+    "=    0 A 9    =",
+    "=   SORTEADO  =",
+    "=             =",
     "==============="
 };
 
-// Funções de callback para interrupção dos butões
-void gpio_callback_btn(uint gpio, uint32_t events);
+char *aperteA[8] = {
+    "===============",
+    "=             =",
+    "=   PRECIONE  =",
+    "=  O BOTAO A  =",
+    "=  PARA MUDAR =",
+    "=   O NUMERO  =",
+    "=             =",
+    "==============="
+};
 
+char *aperteB[8] = {
+    "===============",
+    "=             =",
+    "= PRECIONE O  =",
+    "= BOTAO B PARA=",
+    "= ESCOLHER O  =",
+    "= NUMERO      =",
+    "=             =",
+    "==============="
+};
+
+
+char *iniciado[8] = {
+    "===============",
+    "=             =",
+    "=   PARTIDA   =",
+    "=             =",
+    "=  INICIADA!  =",
+    "=             =",
+    "=             =",
+    "==============="
+};
+
+char *maior[8] = {
+    "===============",
+    "=             =",
+    "=    NUMERO   =",
+    "=             =",
+    "=    MAIOR!   =",
+    "=             =",
+    "=             =",
+    "==============="
+};
+
+char *acertou[8] = {
+    "===============",
+    "=             =",
+    "=   PARABENS  =",
+    "=             =",
+    "=   ACERTOU!  =",
+    "=             =",
+    "=             =",
+    "==============="
+};
+
+char *menor[8] = {
+    "===============",
+    "=             =",
+    "=    NUMERO   =",
+    "=             =",
+    "=    MENOR!   =",
+    "=             =",
+    "=             =",
+    "==============="
+};
+
+// Função callback do temporizador para exibir mensagens
+bool mensagem_inicio_callback(struct repeating_timer *){
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
+    };
+    calculate_render_area_buffer_length(&frame_area);
+
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+
+    // Imprime mensagens
+    int y = 0;
+    for(uint i = 0; i < count_of(aperte); i++){
+        ssd1306_draw_string(ssd, 5, y, aperte[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+
+    return false;  // para execultar só uma vez
+}
+
+// Protótipos das funções
+void btn_pressed(uint gpio, uint32_t events);
 
 int main()
 {
     stdio_init_all();
+    srand(time(NULL));
 
     // Inicializa a semente do gerador de numeros aleatórios
     srand(time(NULL));
@@ -225,6 +286,8 @@ int main()
     // Inicialização das variáveis para trabalhar com a matriz de leds
     pio = pio0;
     sm = configurar_matriz(pio);
+
+    struct repeating_timer timer;
 
     // Inicialização dos gpios
     gpio_init(btnA_pin);
@@ -262,7 +325,7 @@ int main()
     uint8_t ssd[ssd1306_buffer_length];
     memset(ssd, 0, ssd1306_buffer_length);
     render_on_display(ssd, &frame_area);
-
+    
     // Imprimir a mensagem de inicio
     int y = 0;
     for (uint i = 0; i < count_of(inicio); i++)
@@ -273,130 +336,125 @@ int main()
     render_on_display(ssd, &frame_area);
 
     sleep_ms(1000);
-
-    // Imprimir as instruções
-    y = 0;
-    for(uint i = 0; i < count_of(instrucaoA); i++){
-        ssd1306_draw_string(ssd, 5, y, instrucaoA[i]);
-        y += 8;
-    }
-    render_on_display(ssd, &frame_area);
-
-    sleep_ms(1000);
-
-    y = 0;
-    for(uint i = 0; i < count_of(instrucaoB); i++){
-        ssd1306_draw_string(ssd, 5, y, instrucaoB[i]);
-        y += 8;
-    }
-    render_on_display(ssd, &frame_area);
-
-    sleep_ms(1000);
-
-    y = 0;
-    for(uint i = 0; i < count_of(instrucaoC); i++){
-        ssd1306_draw_string(ssd, 5, y, instrucaoC[i]);
-        y += 8;
-    }
-    render_on_display(ssd, &frame_area);
-
-    sleep_ms(1000);
-
-    // Sorteio de um numero aleatorio no intervalo de 0 a 9
-    numero_sorteado = rand() % 10;
-
-    printf("NUmero sorteado: %d\n", numero_sorteado);
-
-    y = 0;
-    for(uint i = 0; i < count_of(sorteio); i++){
-        ssd1306_draw_string(ssd, 5, y, sorteio[i]);
-        y += 8;
-    }
-    render_on_display(ssd, &frame_area);
-
-    sleep_ms(1000);
-
-    y = 0;
-    for(uint i = 0; i < count_of(opcao); i++){
-        ssd1306_draw_string(ssd, 5, y, opcao[i]);
-        y += 8;
-    }
-    render_on_display(ssd, &frame_area);
-
-    sleep_ms(100);
-
-
-    // Configuração de interrupções
-    gpio_set_irq_enabled_with_callback(btnA_pin, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_btn);
-    gpio_set_irq_enabled_with_callback(btnB_pin, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_btn);
-    gpio_set_irq_enabled_with_callback(btn_joy_pin, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_btn);
     
+    y = 0;
+    for (uint i = 0; i < count_of(aperte); i++)
+    {
+        ssd1306_draw_string(ssd, 5, y, aperte[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+
+    // Interrupções
+    gpio_set_irq_enabled_with_callback(btnA_pin, GPIO_IRQ_EDGE_FALL, true, &btn_pressed);
+    gpio_set_irq_enabled_with_callback(btnB_pin, GPIO_IRQ_EDGE_FALL, true, &btn_pressed);
+    gpio_set_irq_enabled_with_callback(btn_joy_pin, GPIO_IRQ_EDGE_FALL, true, &btn_pressed);
+    
+    imprimir_desenho(zero, pio, sm);
+
     while (true) {
-        // Inicializa a semente do gerador de numeros aleatórios
-        srand(time(NULL));
-        numero_sorteado = rand() % 10;
-        printf("Numero sorteado: %d\n", numero_sorteado);
-        sleep_ms(1000);
+        sleep_ms(100);
     }
 }
 
 
+// Corpos das funções
 
-// Funções
-
-void gpio_callback_btn(uint gpio, uint32_t events){
+void btn_pressed(uint gpio, uint32_t events){
     uint32_t current_time = to_ms_since_boot(get_absolute_time());
-
-    // Inicialização do i2c
-    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-
-    // Processo de inicialização completo do OLED SSD1306
-    ssd1306_init();
-
-    // Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
+    
     struct render_area frame_area = {
         start_column : 0,
         end_column : ssd1306_width - 1,
         start_page : 0,
         end_page : ssd1306_n_pages - 1
     };
-
     calculate_render_area_buffer_length(&frame_area);
 
-    // zera o display inteiro
     uint8_t ssd[ssd1306_buffer_length];
     memset(ssd, 0, ssd1306_buffer_length);
-    render_on_display(ssd, &frame_area);
 
-    // debounce de 200 ms btn A
-    if(gpio == btnA_pin && (current_time - last_time_a) > 200){
-        last_time_a = current_time;
-        contador = (contador + 1) % 10;     // Incrementa o contador
+    // debounce
+    if(gpio == btn_joy_pin && (current_time - last_time_joy) > 200){
+        last_time_joy = current_time;
+        inicio_jogo = true;
+    
+        // Gerar um novo numero aleatorio
+        numero_sorteado = rand() % 10;
+    
+        // Imprime mensagens
+        // int y = 0;
+        // for(uint i = 0; i < count_of(iniciado); i++){
+        //     ssd1306_draw_string(ssd, 5, y, iniciado[i]);
+        //     y += 8;
+        // }
+        // render_on_display(ssd, &frame_area);
+
+        int y = 0;
+        for(uint i = 0; i < count_of(sorteio); i++){
+            ssd1306_draw_string(ssd, 5, y, sorteio[i]);
+            y += 8;
+        }
+        render_on_display(ssd, &frame_area);
 
     }
-    // debounce de 200 ms btn B
+    // Debounce btn A
+    else if(gpio == btnA_pin && (current_time - last_time_a) > 200){
+        last_time_a = current_time;
+        if(!inicio_jogo) return;
+
+        // Incrementa o contador
+        contador = (contador + 1) % 10;
+    }
+    // debounce btn B
     else if(gpio == btnB_pin && (current_time - last_time_b) > 200){
         last_time_b = current_time;
-        contador = (contador == 0) ? 9 : (contador - 1);    // Decrementa o contador
-    }
-    // debounce de 200 ms btn joy
-    else if(gpio == btn_joy_pin && (current_time - last_time_joy) > 200){
-        last_time_joy = current_time;
+        struct repeating_timer timer;
 
-        // verifica se o numero é igual ao numero sorteado
-        if(numero_sorteado == contador){
-            printf("PArabens, ganhou\n");
-            ssd1306_draw_string(ssd, 22, 32, "GANHOU");
+        tentativas++;
+
+        // caso seja pressionado sem ter inicado a partida, retornar
+        if(!inicio_jogo) return;
+        
+        
+        // verificando o numero que o usuario escolheu com o sorteado
+        if(contador == numero_sorteado){
+
+            // Imprime a mensagem que ganhou
+            int y = 0;
+            for(uint i = 0; i < count_of(acertou); i++){
+                ssd1306_draw_string(ssd, 5, y, acertou[i]);
+                y += 8;
+            }
             render_on_display(ssd, &frame_area);
+
+            printf("VOCE GANHOU\n");
+
+            add_repeating_timer_ms(3000, mensagem_inicio_callback, NULL, &timer);
+
+            inicio_jogo = false;
+        }
+        else if (contador > numero_sorteado){
+            // Imprime a mensagem que é o numero é menor
+            int y = 0;
+            for(uint i = 0; i < count_of(menor); i++){
+                ssd1306_draw_string(ssd, 5, y, menor[i]);
+                y += 8;
+            }
+            render_on_display(ssd, &frame_area);
+
+            printf("NUMERO MENOR\n");
         }
         else{
-            printf("PERDEU\n");
-            ssd1306_draw_string(ssd, 22, 32, "PERDEU");
+            // Imprime a mensagem que é o numero é menor
+            int y = 0;
+            for(uint i = 0; i < count_of(maior); i++){
+                ssd1306_draw_string(ssd, 5, y, maior[i]);
+                y += 8;
+            }
             render_on_display(ssd, &frame_area);
+
+            printf("NUMERO MAIOR\n");
         }
     }
 
