3f8eb5465a52f214eb0390c785300bc002d1e23f
kauanmateuss <kamateus77@gmail.com>
2025-02-21 21:00:44 -0300

    Adicionei mensagens de inicio de jogo

--- project.c
+++ project.c
@@ -7,16 +7,21 @@
 #include "matriz_leds.h"
 #include "inc/ssd1306.h"
 #include "string.h"
+#include <stdlib.h>
+#include <time.h>
 
 // Definição de variaveis globais e macros
 
 #define btnA_pin 5
 #define btnB_pin 6
 #define buzzerA_pin 21
+#define btn_joy_pin 22
 
 PIO pio;
 uint sm;
 
+uint numero_sorteado; 
+uint contador;
 
 // Pinos para o display
 const uint I2C_SDA = 14;
@@ -25,7 +30,7 @@ const uint I2C_SCL = 15;
 // Variáveis para tratamento de debounce
 static volatile uint32_t last_time_a = 0;
 static volatile uint32_t last_time_b = 0;
-
+static volatile uint32_t last_time_joy = 0;
 
 // matrizes dos numeros para o display
 Matriz_leds_config zero = {
@@ -137,9 +142,9 @@ Matriz_leds_config clear = {
     {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 4
 };
 
-//Matriz_leds_config *numeros[] = {&zero, &um, &dois, &tres, &quatro, &cinco, &seis, &sete, &oito, &nove};
+Matriz_leds_config *numeros[] = {&zero, &um, &dois, &tres, &quatro, &cinco, &seis, &sete, &oito, &nove};
 
-// Mensagem que vão aparecer no display
+// Mensagens que vão aparecer no display
 char *inicio[8] = {
     "===============",
     "               ",
@@ -184,23 +189,54 @@ char *instrucaoC[8] = {
     "==============="
 };
 
-char *sorteio[8] = {
+char *opcao[8] = {
     "===============",
     "               ",
     " SELECIONE UM  ",
     "    NUMERO     ",
     "   DE 0 A 9    ",
-    "   NOS LEDS    ",
+    "   NOS LEDs    ",
     "               ",
     "==============="
 };
+
+char *sorteio[8] = {
+    "===============",
+    "               ",
+    "NUMERO DE 0 A 9",
+    "               ",
+    "   SORTEADO    ",
+    "               ",
+    "               ",
+    "==============="
+};
+
+// Funções de callback para interrupção dos butões
+void gpio_callback_btn(uint gpio, uint32_t events);
+
 
 int main()
 {
     stdio_init_all();
 
+    // Inicializa a semente do gerador de numeros aleatórios
+    srand(time(NULL));
+
+    // Inicialização das variáveis para trabalhar com a matriz de leds
     pio = pio0;
     sm = configurar_matriz(pio);
+
+    // Inicialização dos gpios
+    gpio_init(btnA_pin);
+    gpio_set_dir(btnA_pin, GPIO_IN);
+    gpio_init(btnB_pin);
+    gpio_set_dir(btnB_pin, GPIO_IN);
+    gpio_init(btn_joy_pin);
+    gpio_set_dir(btn_joy_pin, GPIO_IN);
+
+    gpio_pull_up(btnA_pin);
+    gpio_pull_up(btnB_pin);
+    gpio_pull_up(btn_joy_pin);
 
     // Inicialização do i2c
     i2c_init(i2c1, ssd1306_i2c_clock * 1000);
@@ -227,41 +263,142 @@ int main()
     memset(ssd, 0, ssd1306_buffer_length);
     render_on_display(ssd, &frame_area);
 
-    // Imprime uma mensagem de inicio
+    // Imprimir a mensagem de inicio
     int y = 0;
-    for (uint i = 0; i < count_of(instrucaoC); i++)
+    for (uint i = 0; i < count_of(inicio); i++)
     {
+        ssd1306_draw_string(ssd, 5, y, inicio[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+
+    sleep_ms(1000);
+
+    // Imprimir as instruções
+    y = 0;
+    for(uint i = 0; i < count_of(instrucaoA); i++){
+        ssd1306_draw_string(ssd, 5, y, instrucaoA[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+
+    sleep_ms(1000);
+
+    y = 0;
+    for(uint i = 0; i < count_of(instrucaoB); i++){
+        ssd1306_draw_string(ssd, 5, y, instrucaoB[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+
+    sleep_ms(1000);
+
+    y = 0;
+    for(uint i = 0; i < count_of(instrucaoC); i++){
         ssd1306_draw_string(ssd, 5, y, instrucaoC[i]);
         y += 8;
     }
     render_on_display(ssd, &frame_area);
 
+    sleep_ms(1000);
+
+    // Sorteio de um numero aleatorio no intervalo de 0 a 9
+    numero_sorteado = rand() % 10;
+
+    printf("NUmero sorteado: %d\n", numero_sorteado);
+
+    y = 0;
+    for(uint i = 0; i < count_of(sorteio); i++){
+        ssd1306_draw_string(ssd, 5, y, sorteio[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+
+    sleep_ms(1000);
+
+    y = 0;
+    for(uint i = 0; i < count_of(opcao); i++){
+        ssd1306_draw_string(ssd, 5, y, opcao[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+
+    sleep_ms(100);
 
 
+    // Configuração de interrupções
+    gpio_set_irq_enabled_with_callback(btnA_pin, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_btn);
+    gpio_set_irq_enabled_with_callback(btnB_pin, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_btn);
+    gpio_set_irq_enabled_with_callback(btn_joy_pin, GPIO_IRQ_EDGE_FALL, true, &gpio_callback_btn);
+    
     while (true) {
-        imprimir_desenho(zero, pio, sm);
-        sleep_ms(1000);
-        imprimir_desenho(clear, pio, sm);
+        // Inicializa a semente do gerador de numeros aleatórios
+        srand(time(NULL));
+        numero_sorteado = rand() % 10;
+        printf("Numero sorteado: %d\n", numero_sorteado);
         sleep_ms(1000);
     }
 }
 
 
-// Função para escrever uma mensagem no display
-// void print_display(char *text, size_t count, struct render_area *frame_area){
 
-//     // zera o display inteiro
-//     uint8_t ssd[ssd1306_buffer_length];
-//     memset(ssd, 0, ssd1306_buffer_length);
-//     render_on_display(ssd, &frame_area);
+// Funções
 
-// restart:
+void gpio_callback_btn(uint gpio, uint32_t events){
+    uint32_t current_time = to_ms_since_boot(get_absolute_time());
 
-//     int y = 0;
-//     for (uint i = 0; i < count_of(text); i++)
-//     {
-//         ssd1306_draw_string(ssd, 5, y, text[i]);
-//         y += 8;
-//     }
-//     render_on_display(ssd, &frame_area);
-// }
+    // Inicialização do i2c
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    // Processo de inicialização completo do OLED SSD1306
+    ssd1306_init();
+
+    // Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
+    };
+
+    calculate_render_area_buffer_length(&frame_area);
+
+    // zera o display inteiro
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+    // debounce de 200 ms btn A
+    if(gpio == btnA_pin && (current_time - last_time_a) > 200){
+        last_time_a = current_time;
+        contador = (contador + 1) % 10;     // Incrementa o contador
+
+    }
+    // debounce de 200 ms btn B
+    else if(gpio == btnB_pin && (current_time - last_time_b) > 200){
+        last_time_b = current_time;
+        contador = (contador == 0) ? 9 : (contador - 1);    // Decrementa o contador
+    }
+    // debounce de 200 ms btn joy
+    else if(gpio == btn_joy_pin && (current_time - last_time_joy) > 200){
+        last_time_joy = current_time;
+
+        // verifica se o numero é igual ao numero sorteado
+        if(numero_sorteado == contador){
+            printf("PArabens, ganhou\n");
+            ssd1306_draw_string(ssd, 22, 32, "GANHOU");
+            render_on_display(ssd, &frame_area);
+        }
+        else{
+            printf("PERDEU\n");
+            ssd1306_draw_string(ssd, 22, 32, "PERDEU");
+            render_on_display(ssd, &frame_area);
+        }
+    }
+
+    imprimir_desenho(*numeros[contador], pio, sm);
+}
